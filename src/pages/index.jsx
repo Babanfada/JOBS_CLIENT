963ba361aacf469fbbd7a41586f7f4d16af95805
Tolani <110635435+Babanfada@users.noreply.github.com>
2023-03-25 11:52:54 +0000

    ommit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,10 @@
-// import { Inter } from "next/font/google";
-import styles from "../styles/home.module.scss";
+import styles from "@/styles/home.module.scss";
 import Header from "@/components/Head";
 import Nav from "@/components/Navigation";
 import Textarea from "@/components/Textarea";
 import Image from "next/image";
 import main from "../asset/main.svg";
-import { red } from "@mui/material/colors";
+// import { red } from "@mui/material/colors";
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
