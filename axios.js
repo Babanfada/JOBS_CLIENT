c813e255cd79b3fdec264382e6482a3f5c72d838
Tolani <110635435+Babanfada@users.noreply.github.com>
2023-03-25 11:23:28 +0000

    commit

--- axios.js
+++ axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:5000/api/v1";
+axios.defaults.baseURL = "https://jobs-api-users-info-api.vercel.app/api/v1/";
+// axios.defaults.baseURL = "http://localhost:5000/api/v1";
 axios.interceptors.request.use(function (req) {
   const user = localStorage.getItem("user");
 
